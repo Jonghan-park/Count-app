81adf4c9fc7ba6e50ede69335778ad62798b19ed
jonghan-park <jonghan.park@edu.sait.ca>
2022-01-28 15:54:33 -0700

    Update increment & decrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,42 @@ class Counter extends Component {
     count: 1
   };
 
+//   constructor(){
+//       super();
+//       this.handleIncrement =  this.handleIncrement.bind(this);
+//   }
+
+  handleIncrement = () => {
+      //console.log('Increment clicked.', this);
+      //this.state.count++;
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    handleDecrement = () => {
+        this.setState({ count: this.state.count - 1 })
+    }
+
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+        <div>
+            <span className={this.getAlertClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-3">Increment</button>
+            <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-5">Decrement</button>
+        </div>
+
     );
+
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  getAlertClasses(){
+      let classes = "alert m-2 alert-";
+      classes += this.state.count === 0 ? "warning" : "primary";
+      return classes;
+  }
+
+  formatCount(){
+      const { count } = this.state;
+      return count === 0 ? 'Zero' : count;
   }
 }
 
